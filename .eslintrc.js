09d1fb46bbeca1fd2b4f005ec63d200cc99e23cf
zhulinqingtian <1399873403@qq.com>
2019-08-05 17:49:01 +0800

    add: eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -24,6 +24,28 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': 0,
+    'linebreak-style': 0,
+    'arrow-body-style': 0,
+    'no-else-return': 0,
+    "object-shorthand": 0,
+    'semicolon': 0,
+    'prefer-template': 0,
+    'operator-assignment': 0,
+    'func-names': 0,
+    'no-param-reassign': 0,
+    'prefer-destructuring': 0,
+    'no-unused-expressions': 0,
+    'no-shadow': 0,
+    'global-require': 0,
+    'object-curly-spacing': 0,
+    'prefer-arrow-callback': 0,
+    'no-trailing-spaces': 0,
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-alert': 0,
+    'no-nested-ternary': 0,
+    "quotes": [1, "single"]
   }
 }
